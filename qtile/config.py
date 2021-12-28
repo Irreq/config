219aa1cb990e7b2458243466f11bc48ba1ac1310
Irreq <irreq@protonmail.com>
2021-12-29 00:46:45 +0100

    minor changes

--- qtile/config.py
+++ qtile/config.py
@@ -1,43 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# File name: main.py
+# File name: config.py
 # Description: Irreq's Qtile Config File
 # Author: irreq (irreq@protonmail.com)
 # Date: 02/11/2021
-
-"""
-USER NOTICE
-
-This is only the config for the 'window-manager' settings, eg. keybinds.
-All other stuff like statusbar are located in a different file. See down below.
-
-"""
 
 from libqtile import bar, hook, pangocffi, layout, widget
 
 from libqtile.command import lazy
 
 from libqtile.config import Group, Key, Screen
+from libqtile.config import EzClick as Click  # For macros on Logitech M570
 
-# For macros on Logitech M570
-from libqtile.config import EzClick as Click
-
-from libqtile.widget.base import ORIENTATION_HORIZONTAL
-from libqtile.widget.base import _TextBox as BaseTextBox
-from libqtile.widget.base import InLoopPollText, ThreadPoolText
+from libqtile.widget.base import _TextBox, ThreadPoolText, ORIENTATION_HORIZONTAL
 
 # For every other function
 import os, sys
 path = os.path.expanduser("~/github/programs/de")
 sys.path.append(path)
 
-import socket
-
 try:
-    from main import programs, update_status_wrapper, launch, test_tts
+    from main import programs, launch, test_tts
 except ModuleNotFoundError:
     print("[USER] module 'main' could not be found, does it exist?")
+
+import socket
 
 try:
     from main import program_ul as program
@@ -48,6 +36,51 @@ except Exception as e:
             return programs[p]
 
 # CONFIG
+
+alias = {
+    # Programs
+    "alacritty": "alacritty",
+    "discord": "Discord",
+
+    # Programming
+    "atom": "atom",
+    "nvim": "nvim",
+    "vim": "vim",
+    "python3": "alacritty",
+
+    # Web
+    "firefox": "firefox",
+    "github": "firefox https://github.com/Irreq",
+    "youtube": "https://www.youtube.com/results?search_query=QUERY",
+
+    # System (be careful, some stuff might break)
+    "update": "sudo xbps-install -Su",
+    "reboot": "sudo reboot now",
+    "shutdown": "sudo shutdown -h now",
+
+    "_tester": "(alacritty &)",
+
+    # Audio
+    "vol_up": "amixer -q sset Master 10%+",
+    "vol_down": "amixer -q sset Master 10%-",
+    "pause": "python3 -q /home/irreq/github/programs/audio.py toggle",
+    "gesture_audio": "python3 -q /home/irreq/github/programs/handVolumeController.py",
+    "pavucontrol": "pavucontrol",
+    "spotify": "spotify -no-zygote",
+
+    # Meta
+    "open": "atom",
+    "filebrowser": "thunar",
+    "tts": "sam",  # requires 'SAM' as /bin/sam
+    "searchbrowser": "firefox",
+    "search": "firefox https://duckduckgo.com/?q=QUERY&ia=web", # QUERY is what you type after search
+    "terminal": "alacritty",
+    "keyboard": "setxkbmap se",
+    "wifi": "sudo wpa_supplicant -B -iwlo1 -c/etc/wpa_supplicant/wpa_supplicant-wlo1.conf",
+    "screen_hdmi": "xrandr --output VGA-0 --off --output LVDS --off --output HDMI-0 --mode 1920x1200 --pos 0x0 --rotate normal",
+    "screen_vga": "xrandr --output HDMI-0 --off --output LVDS --off --output VGA-0 --mode 1920x1200 --pos 0x0 --rotate normal",
+    "screen_vga_thinkpad": "xrandr --output HDMI-0 --off --output LVDS1 --off --output VGA1 --mode 1920x1200 --pos 0x0 --rotate normal",
+}
 
 MOD = "mod4"  # The 'Super' key, aka Windows-key
 FONT = "PxPlus HP 100LX 10x11"
@@ -65,32 +98,66 @@ class Colors:
 
 
 class MetricsListener(ThreadPoolText):
-    def __init__(self, udp_ip="localhost", udp_port=28441, **config):
+    def __init__(self, udp_ip="localhost", udp_port=28441, **kwargs):
         self.udp_ip = udp_ip
         self.udp_port = udp_port
+        self.config = kwargs
+        self.sock = False
+        self.timeout_count = 0
+
+        self.connect()
+
+        if self.is_socket_closed(self.sock):
+            self.sock = False
+
+        super().__init__("Booting...".format(self.udp_ip, self.udp_port), **self.config)
+        self.update_interval = 1
+
+
+    def is_socket_closed(self, sock):
+        try:
+            # Will try to read bytes without blocking and also without removing them from buffer (peek only)
+            data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
+            if len(data) == 0:
+                return True
+        except BlockingIOError:
+            return False  # Socket is open and reading from it would block
+        except ConnectionResetError:
+            return True  # Socket was closed for some other reason
+        except Exception as e:
+            logger.exception("unexpected exception when checking if a socket is closed")
+            return False
+        return False
+
+
+    def connect(self):
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             # [Errno 98] Address already in use, https://stackoverflow.com/questions/4465959/python-errno-98-address-already-in-use
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.sock.bind((udp_ip, udp_port))
+            self.sock.bind((self.udp_ip, self.udp_port))
         except Exception as e:
             print(e)
-            sock = False
-
-        finally:
-            super().__init__("Nothing on ('{}' {}) DO NOT RESTART QTILE UNTIL METRIC DEAMON IS RUNNING".format(self.udp_ip, self.udp_port), **config)
-            self.update_interval = 1
+            self.sock = False
 
     def poll(self):
-        if self.sock:
-            # buffer size is 1024 bytes
-            data, addr = self.sock.recvfrom(1024)
-            return data.decode("utf8")
+        if not self.is_socket_closed(self.sock):
+            data = "Nothing received for %ds. Is pymetricsd.py running?" % (self.timeout_count * 5)
+            self.sock.settimeout(5)
+            try:
+                # buffer size is 1024 bytes
+                data, addr = self.sock.recvfrom(1024)
+                data = data.decode("utf8")
+                self.timeout_count = 0
+            except:
+                self.timeout_count += 1
+            self.sock.settimeout(None)
+            return data
         else:
             return "Socket Error"
 
 
-class CustomBaseTextBox(BaseTextBox):
+class CustomBaseTextBox(_TextBox):
     defaults = [
         ("text_shift", 0, "Shift text vertically"),
     ]
@@ -107,7 +174,7 @@ class CustomBaseTextBox(BaseTextBox):
         self.drawer.clear(self.background or self.bar.background)
         self.layout.draw(
             self.actual_padding or 0,
-            int(self.bar.height / 2.0 - self.layout.height / 2.0) + 1 + self.text_shift,  # here
+            int(self.bar.height / 2.0 - self.layout.height / 2.0) + 1 + self.text_shift,
         )
         self.drawer.draw(offsetx=self.offsetx, width=self.width)
 
@@ -400,6 +467,12 @@ def create_widgets():
     yield CustomWindowName(
         padding=20
     )
+    # yield metricsListener(
+    #     ThreadPoolText,
+    #     font=FONT,
+    #     fontsize=FONTSIZE,
+    #     foreground=Colors.highlight_text,
+    # )
     yield MetricsListener(
         font=FONT,
         fontsize=FONTSIZE,
