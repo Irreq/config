166b83ac875fdf72906679367d470399795a31a1
Irreq <irreq@protonmail.com>
2023-08-12 23:24:13 +0200

    Update idle.sh

--- sway/idle.sh
+++ sway/idle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-swayidle -w \
+swayidle \
         timeout 60 'swaylock -f -c 000000' \
-        timeout 60 'hyprctl dispatch dpms off' \
+        timeout 70 'hyprctl dispatch dpms off' \
                 resume 'hyprctl dispatch dpms on' \
         before-sleep 'swaylock -f -c 000000'
